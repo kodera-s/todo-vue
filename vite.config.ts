7fc3587fc4d822299212d2abe6f77f3d65007725
s-kodera <s.m.k-do40@outlook.com>
2023-04-04 18:09:15 +0900

    build設定変更

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  base: './',
   build: {
-    outDir: './docs'
+    outDir: './docs',
+    assetsDir: './'
   },
 })
