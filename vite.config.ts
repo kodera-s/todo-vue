d3dc8d48572fd8d5b916afab7b351e89cc3ee871
s-kodera <s.m.k-do40@outlook.com>
2023-04-04 18:15:31 +0900

    buildURL設定変更

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './',
+  base: './todo-vue/',
   build: {
     outDir: './docs',
     assetsDir: './todo-vue/'
