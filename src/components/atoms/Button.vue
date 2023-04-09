e9cb1d78b9033fbc3571ba3f6e8a25c9442551fe
s-kodera <s.m.k-do40@outlook.com>
2023-04-09 12:43:37 +0900

    表示変更

---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import { defineProps } from 'vue'
+  const props = defineProps({
+    buttonSet: {
+      type: Number,
+      default: 0
+    },
+    isDisable: {
+      type: Boolean,
+      default: false
+    }
+  })
+
+  const displayButton = [
+    { buttonText: '追加', buttonColor: 'btn-primary'},
+    { buttonText: '作業中', buttonColor: 'btn-warning'},
+    { buttonText: '完了', buttonColor: 'btn-success'},
+    { buttonText: '削除', buttonColor: 'btn-danger'}
+  ]
+
+  const emit = defineEmits(['onClick'])
+  const emitClick = () => {
+    emit('onClick')
+  }  
+</script>
+
+<template>
+  <button class="btn" :class="displayButton[props.buttonSet].buttonColor" :disabled="isDisable" @click="emitClick">
+    {{ displayButton[props.buttonSet].buttonText }}
+  </button>
+</template>
+
+<style lang="scss" scoped>
+</style>
--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,14 +1,36 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
+  import { ref, computed } from 'vue'
 
   import InputText from '../components/atoms/InputText.vue'
+  import InputButton from '../components/atoms/Button.vue'
 
   const isListCheck = ref<Boolean>(false)
   const newItem = ref<String>()
   const todos = ref([])
+  const itemID = ref<Number>(0)
   const inputText = ref()
+  const displayStatus = ref<String>('all')
   
-  console.log('todos', todos.value.length)
+  const computedList = computed(() => {
+    const list = []
+    if(displayStatus.value === "inProgress") {
+      todos.value.forEach(element => {
+        if(!element.isDone) {
+          list.push(element)
+        }
+      })
+      return list
+    }
+    if(displayStatus.value === "finished") {
+      todos.value.forEach(element => {
+        if(element.isDone) {
+          list.push(element)
+        }
+      })
+      return list
+    }
+    return todos.value
+  })
 
   // inputTextからemitを受け取り
   const emitInputValue = (props) => {
@@ -18,8 +40,9 @@
   // リスト追加処理
   const addItem = () => {
     if (!newItem.value) return
+    itemID.value ++
     const todo = {
-      checked: false,
+      id: itemID.value,
       item: newItem.value,
       isDone: false
     }
@@ -36,52 +59,57 @@
     todos.value.splice(index, 1)
   }
 
-  const changeCheckAll = () => {
-    if (isListCheck.value) {
-      console.log('true')
-      todos.value.forEach(element =>{
-        element.checked = true
-      })
-    } else {
-      console.log('false')
-      todos.value.forEach(element =>{
-        element.checked = false
-      })
-    }
-
+  // status変更
+  const changeStatus = (index) => {
+    todos.value[index].isDone = !todos.value[index].isDone
   }
-
 </script>
 
 <template>
   <div class="todo">
-    <div class="input d-flex container-fluid mb-5">
+    <div class="input d-flex align-items-center container-fluid mb-5">
       <div class="input__text col-11 pe-3">
         <InputText ref="inputText" @inputText=emitInputValue />
       </div>
-      <button class="input__button col" @click="addItem">
-        Add
-      </button>
+      <InputButton :buttonSet=0 :isDisable="!newItem" @onClick="addItem" />
+    </div>
+    <div class="container-fluid mb-5">
+      <dl class="d-flex container-fluid">
+        <dt class="col-1">
+          表示
+        </dt>
+        <dd class="d-flex">
+          <div class="me-5">
+            <input type="radio" name="display" id="all" value="all" v-model="displayStatus">
+            <label for="all">すべて</label>
+          </div>
+          <div class="me-5">
+            <input type="radio" name="display" id="inProgress" value="inProgress" v-model="displayStatus">
+            <label for="inProgress">作業中</label>
+          </div>
+          <div>
+            <input type="radio" name="display" id="finished" value="finished" v-model="displayStatus">
+            <label for="finished">完了</label>
+          </div>
+        </dd>
+      </dl>
+
     </div>
     <ul class="todo-list container-fluid">
-      <li class="todo-list__item border-bottom">
-        <div class="todo-list__item--checkbox">
-          <input type="checkbox" v-model="isListCheck" @change="changeCheckAll" :disabled="todos.length === 0">
-        </div>
-        <div class="todo-list__item--number">No.</div>
-        <div class="todo-list__item--thing">Thing</div>
-        <div class="todo-list__item--status">Status</div>
-        <div class="todo-list__item--button"></div>
+      <li class="border-bottom d-flex align-items-center pb-3">
+        <div class="col-1 ps-3">ID.</div>
+        <div class="col-9">Thing</div>
+        <div class="col-1 d-flex justify-content-center">Status</div>
+        <div class="col-1"></div>
       </li>
-      <li v-for="(todo, index) in todos" :key="index" class="todo-list__item py-2">
-        <div class="todo-list__item--checkbox">
-          <input type="checkbox" v-model="todo.checked">
+      <li v-for="(todo, index) in computedList" :key="index" class="d-flex align-items-start py-2 todo-list__item">
+        <div class="col-1 ps-3 pt-2">{{todo.id}}</div>
+        <div class="col-9 text-break pe-5 pt-2">{{todo.item}}</div>
+        <div class="col-1 d-flex justify-content-center">
+          <InputButton :buttonSet="todo.isDone ? 2 : 1" :isDisable="false" @onClick="changeStatus(index)" />
         </div>
-        <div class="todo-list__item--number">{{index + 1}}</div>
-        <div class="todo-list__item--thing">{{todo.item}}</div>
-        <div class="todo-list__item--status">{{todo.isDone}}</div>
-        <div class="todo-list__item--button">
-          <button @click="deleteItem(index)">Delete</button>
+        <div class="col-1 d-flex justify-content-center">
+          <InputButton :buttonSet=3 :isDisable="false" @onClick="deleteItem(index)" />
         </div>
       </li>
     </ul>
@@ -91,27 +119,9 @@
 <style lang="scss" scoped>
 .todo-list {
   &__item {
-    display: flex;
-    &--checkbox {
-      width: 5%;
-      text-align: center;
-    }
-    &--number {
-      width: 10%;
-    }
-    &--thing {
-      width: 70%;
-    }
-    &--status {
-      width: 10%;
-    }
-    &--button {
-      width: 5%;
-    }
-    &--title {
-      color: #f00;
+    &:hover {
+      background-color: #cfe2ff;
     }
   }
 }
-
 </style>
